72739803c08956fc9e20a038efff9de261ac50a6
abbas jafari <realabja@gmail.com>
2020-08-21 23:50:37 +0200

    ...

--- pagegenerator/pagegenerator.py
+++ pagegenerator/pagegenerator.py
@@ -26,17 +26,17 @@ post_preview = read("post-preview-template.html")
 
 def process(raw_template):
     for index, line in enumerate(raw_template):
+        raw_template[index] = line.replace("{folder_name(title)}", folder_name(title))
         if re.search("<!-- content insertion -->", line):
             content_type = re.search(r"type==[1-zA-Z-]*", raw_template[index + 1]).group()
             content_type = content_type.replace("type==", "")
-            raw_template.pop(index)
-            raw_template.pop(index)
-            raw_template.insert(index, globals()[content_type])
-    processed_page = raw_template
-    return processed_page
 
-        # content_holder = (index, content_type)
-        # content_holders.append(content_holder)
+            raw_template.insert(index, globals()[content_type])
+
+    return raw_template
+
+# content_holder = (index, content_type)
+# content_holders.append(content_holder)
 
 
 try:
@@ -45,11 +45,9 @@ except:
     print("error while creating directory")
 
 
-
 with open(f"{folder_name(title)}/index.html", "w")as processed_page:
     processed_page.writelines(process(post_template))
 
 
 with open(f"{folder_name(title)}/post-preview.html", "w")as processed_page:
     processed_page.writelines(process(post_preview))
-
